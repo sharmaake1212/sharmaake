d857b43a552d1407f3eb51fb4edf99dd0bb5a408
sharmaake1212 <sharmaakecsalaam@gmail.com>
2024-06-29 11:01:06 +0300

    Add files via upload

---
+++ Quiz1.java
@@ -0,0 +1,32 @@
+package c1221026;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Quiz1 {
+    public static void main(String[] args) {
+        int[] inputArray = {1, 2, 2, 3, 4, 4, 5};
+        int[] resultArray = removeDuplicates(inputArray);
+        System.out.println(Arrays.toString(resultArray));
+    }
+
+    public static int[] removeDuplicates(int[] array) {
+        List<Integer> uniqueList = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            if (!uniqueList.contains(array[i])) {
+                uniqueList.add(array[i]);
+            }
+        }
+
+        int[] uniqueArray = new int[uniqueList.size()];
+        for (int i = 0; i < uniqueList.size(); i++) {
+            uniqueArray[i] = uniqueList.get(i);
+        }
+
+        return uniqueArray;
+    }
+
+
+}
+
